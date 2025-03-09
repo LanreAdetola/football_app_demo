e1e03328c77c75369e90f80bd39b6db5542cd871
LanreAdetola <100709089+LanreAdetola@users.noreply.github.com>
2025-03-09 21:12:54 +0100

    V 2.0

--- views/about_me.py
+++ views/about_me.py
@@ -2,26 +2,30 @@ import streamlit as st
 
 from forms.contact import contact_form
 
+@st.dialog("Contact Me")
 def show_contact_form():
     contact_form()
 
 # -- Me --
-col1, col2 = st.columns(2)
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 with col1:
     st.image("assets/profilepic.png", width=230)
 with col2:
-    st.title("Lanre Adetola")
+    st.title("Lanre Adetola", anchor=False)
     st.write(
-        "Data Analyst/Data Scientist Intern, seeking to assist enterprises by supporting data-driven decision-making."
+        "Data Scientist Student Intern, seeking to assist enterprises by supporting data-driven decision-making."
     )
-    if st.button("Contact Me"):
+    st.write(
+        "**Email**: r0913836@student.thomasmore.be"
+    )
+    if st.button(" Contact Me"):
         show_contact_form()
 
 
 #--- Experience---
 
 st.write("\n")
-st.subheader("Experience & Qualifications")
+st.subheader("Experience & Qualifications", anchor=False)
 st.write(
     """
     - Experienced in deriving actionable insights from complex data sets
@@ -36,12 +40,12 @@ st.write(
 #---Skills---
 
 st.write("\n")
-st.subheader("Hard Skills")
+st.subheader("Hard Skills", anchor=False)
 st.write(
     """
-    - **Programming Languages**: Python (with libraries such as **Scikit-learn**, **Pandas**), **SQL**, **C#**
-    - **Data Visualization**: Expertise in **Power BI**, **MS Excel**, **Matplot**, **Plotly** for creating interactive and insightful visualizations
-    - **Machine Learning & Modeling**: Experience with **Logistic Regression**, **Linear Regression**, **Decision Trees** for predictive modeling
-    - **Databases**: Proficient in **MySQL**, **LiteDB**, **MongoDB** for data storage and management
+    - **Programming Languages**: Python (with libraries such as **Scikit-learn**, **Pandas**), **SQL**
+    - **Data Visualization**: Profficent in **Matplotlib**, **Plotly**, **Seaborn** for creating interactive and insightful visualizations
+    - **Machine Learning & Modeling**: Experience with **Logistic Regression**, **Linear Regression**, **Decision Trees** etc for predictive modeling
+    - **Databases**: Proficient in **MySQL**, **LiteDB**, **MongoDB** for data storage and management, with hands-on experience in Azure SQL Database, Azure Cosmos DB, and Azure Data Lake for cloud-based data solutions and scalability.
     """
 )
